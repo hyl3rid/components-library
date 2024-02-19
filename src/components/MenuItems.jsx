5a571fd46a5d28e5f3c34aa352696233102e6028
Clifford Bamert <cliff.bam92@gmail.com>
2024-02-19 14:10:14 -0600

    add icon and links to second navbar, enable close on click when outside element

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,25 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Dropdown from './Dropdown';
 
 const MenuItems = ({ items }) => {
     const [dropdown, setDropdown] = useState(false);
 
+    let ref = useRef();
+
+    useEffect(() => {
+        const handler = (event) => {
+         if (dropdown && ref.current && !ref.current.contains(event.target)) {
+          setDropdown(false);
+         }
+        };
+        document.addEventListener("mousedown", handler);
+        document.addEventListener("touchstart", handler);
+        return () => {
+         // Cleanup the event listener
+         document.removeEventListener("mousedown", handler);
+         document.removeEventListener("touchstart", handler);
+        };
+       }, [dropdown]);
+
     return (
-        <li>
+        <li class="navigation-bar__link-item" onClick={() => setDropdown((prev) => !prev)}  ref={ref}>
             {items.submenu ? (
                 <>
                     <button 
                     role="button" 
                     class="navigation-bar__submenu"
                     aria-expanded={dropdown ? "true" : "false"}
-                    onClick={() => setDropdown((prev) => !prev)}
                     >
                         {items.title}
                     </button>
+                
+                    {items.downChevron && <div class="navigation-bar__down-chevron">{items.downChevron}</div>}
                     <Dropdown submenus={items.submenu} dropdown={dropdown}/>
                 </>
             ) : (
-                <a href="{items.url}">{items.title}</a>
+                <>
+                    <a href="{items.url}">{items.title}</a>
+                </>
             )}
         </li>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,27 +20,19 @@ function Navbar({position = "default"}) {
     return (
       <nav class="navigation-bar navigation-bar--center">
           <ul class="navigation-bar__nav-links navigation-bar__nav-links--left">
-            <li class="navigation-bar__dropdown-menu">
-              <span>
-                NavLink
-              </span>
-              <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5.84615 6L0 0H12L5.84615 6Z" fill="white"/>
-              </svg>
-            </li>
-            <li>NavLink</li>
+            {menuItemsData.map((menu, index) => {
+              if (index < 3) {
+                return <MenuItems items={menu} key={index} />;
+              }
+            })}
           </ul>
           <h1 class="navigation-bar__logo">LOGO</h1>
           <ul class="navigation-bar__nav-links navigation-bar__nav-links--right">
-            <li>NavLink</li>
-            <li class="navigation-bar__login">
-              <span>
-                Login
-              </span>
-              <svg width="18" height="25" viewBox="0 0 18 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4.28621 13.9846C-0.972573 11.222 -0.41915 0 9 0C18.5235 0 18.3195 10.038 13.8142 13.9776C13.6355 14.1338 13.5771 14.3932 13.6825 14.6059C15.1468 17.5618 16.2935 19.7406 17.1412 21.2893C17.9058 22.6862 16.8799 24.5 15.2875 24.5H2.17433C0.702648 24.5 -0.241776 22.9776 0.458587 21.6833C1.39218 19.9579 2.75915 17.5469 4.5166 14.7545C4.68505 14.4868 4.56618 14.1317 4.28621 13.9846Z" fill="white"/>
-              </svg>
-            </li>
+            {menuItemsData.map((menu, index) => {
+              if (index >= 3) {
+                return <MenuItems items={menu} key={index} />;
+              }
+            })}
           </ul>
       </nav>
     );
